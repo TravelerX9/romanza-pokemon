b98f1eabb1caa29f92ba3bb8d15bc8ba9d95c1ef
Romanza <fontanafreedom9@gmail.com>
2025-07-07 13:51:06 +0800

    Update list-pokemon.tsx

--- src/home/list/list-pokemon.tsx
+++ src/home/list/list-pokemon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from './card-pokemon';
 import { usePokemonList } from '../../lib/hooks/usePokemonList';
 import { useRouter } from 'next/navigation';
@@ -10,6 +10,8 @@ interface ListPokemonProps {
 const ListPokemon: React.FC<ListPokemonProps> = ({ keyword }) => {
   const { data, isLoading, error } = usePokemonList();
   const router = useRouter();
+  const [rows, setRows] = useState(2); // tampilkan 2 baris awal
+  const cardsPerRow = 4;
 
   // Filter langsung pakai keyword dari props
   const filtered = data
@@ -22,17 +24,20 @@ const ListPokemon: React.FC<ListPokemonProps> = ({ keyword }) => {
     router.push(`/power/power-pokemon-page?id=${id}`);
   };
 
+  const visiblePokemons = filtered.slice(0, rows * cardsPerRow);
+  const canLoadMore = visiblePokemons.length < filtered.length;
+
   return (
     <>
-      <div className='mx-auto box-border flex min-h-[530px] w-[230px] flex-col gap-2 rounded-3xl bg-white px-6 py-10 md:min-h-[2530px] md:w-[1200px]'>
+      <div className='mx-auto box-border flex min-h-[530px] w-[230px] flex-col gap-2 rounded-3xl bg-white px-6 py-10 md:min-h-[1000px] md:w-[1200px]'>
         <h2 className='font-inter mt-0 mb-8 self-start text-[22px] font-bold text-[#222] not-italic md:mt-26 md:text-[32px]'>
           <b>List Pokemon</b>
         </h2>
         <div className='grid w-full grid-cols-1 items-center justify-items-center gap-10 md:grid-cols-4'>
           {isLoading && <div>Loading...</div>}
           {error && <div>Error loading list</div>}
-          {filtered &&
-            filtered.map((pokemon) => (
+          {visiblePokemons &&
+            visiblePokemons.map((pokemon) => (
               <div
                 key={pokemon.id}
                 onClick={() => handleCardClick(pokemon.id.toString())}
@@ -47,9 +52,14 @@ const ListPokemon: React.FC<ListPokemonProps> = ({ keyword }) => {
               </div>
             ))}
         </div>
-        <button className='right-0 left-0 mx-auto mt-10 h-[52px] w-[200px] rounded-full border border-gray-300 bg-white text-lg font-medium text-[#181C32] transition hover:bg-gray-100'>
-          Load More
-        </button>
+        {canLoadMore && (
+          <button
+            className='right-0 left-0 mx-auto mt-10 h-[52px] w-[200px] rounded-full border border-gray-300 bg-white text-lg font-medium text-[#181C32] transition hover:bg-gray-100'
+            onClick={() => setRows((r) => r + 2)}
+          >
+            Load More
+          </button>
+        )}
       </div>
     </>
   );

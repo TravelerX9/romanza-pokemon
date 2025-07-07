fdc9862389c700f8105aad1e6008edb3822fe216
Romanza <fontanafreedom9@gmail.com>
2025-07-07 13:59:02 +0800

    Update navbar-search.tsx

--- src/home/partials/navbar-search.tsx
+++ src/home/partials/navbar-search.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const NavbarSearch = () => (
-  <header className='mb-18 flex w-full items-center justify-between bg-white shadow'>
+  <header className='mb-18 flex w-full items-center justify-between bg-white'>
     <div className='flex items-center gap-3' style={{ marginLeft: '100px' }}>
       <img src='/icons/pokeball.svg' alt='Pokedex Logo' className='h-6 w-6' />
       <span className='font-inter text-xl font-medium tracking-wide text-gray-900'>

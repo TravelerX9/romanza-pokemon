f534423a98a220eace8d1a15a8cd26cb0d74f5d4
Romanza <fontanafreedom9@gmail.com>
2025-07-07 13:07:04 +0800

    Update footer.tsx

--- src/home/partials/footer.tsx
+++ src/home/partials/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer: React.FC = () => (
-  <footer className='box-border flex h-[120px] w-[1440px] items-center bg-white -pl-1 md:pl-10'>
+  <footer className='-pl-1 box-border flex h-[120px] w-[100px] items-center bg-white md:w-[1440px] md:pl-10'>
     <div className='absolute left-[16%] flex items-center'>
       {/* Pokeball logo */}
       <img
@@ -9,7 +9,7 @@ const Footer: React.FC = () => (
         alt='Pokeball'
         width={32}
         height={32}
-        className='mr-2 z-10 block h-8 w-8 md:h-10 md:w-10'
+        className='z-10 mr-2 block h-8 w-8 md:h-10 md:w-10'
       />
       <span className='font-inter mr-4 text-2xl font-bold text-[#222] md:text-3xl'>
         Pokedex

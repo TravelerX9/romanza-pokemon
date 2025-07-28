19e2364f1b64654523bb6eadb7bd86782ba68855
Romanza <fontanafreedom9@gmail.com>
2025-07-29 04:21:38 +0800

    Update pokemon.ts

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,7 +1,3 @@
 import { Pokemon } from '../lib/types/pokemon';
 
-export async function fetchPokemon(name: string): Promise<Pokemon> {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  if (!res.ok) throw new Error('Pokemon not found');
-  return res.json();
-}
+

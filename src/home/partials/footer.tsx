34099dcb73553bed9e105a25f643e7a7c71cf6e8
Romanza <fontanafreedom9@gmail.com>
2025-08-01 10:17:22 +0800

    Update footer.tsx

--- src/home/partials/footer.tsx
+++ src/home/partials/footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC = () => (
       <span className='font-inter text-base font-normal text-gray-400'>
         Copyright ©2025 Pokedex
       </span>
-       <Link
+       <a
         href='https://www.linkedin.com/in/romanza-hendra-bayu-40819211a/'
         target='_blank'
         rel='noopener noreferrer'
@@ -32,7 +32,7 @@ const Footer: React.FC = () => (
         >
           <path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.026-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.967v5.7h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.838-1.563 3.036 0 3.6 2.001 3.6 4.601v5.595z' />
         </svg>
-      </Link>
+      </a>
     </div>
   </footer>
 );
